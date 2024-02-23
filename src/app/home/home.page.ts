e2806e9a687fdbd57e30453f4ade50d5d5dea39f
MOTTIN Benoit <BMOTTIN@fim-online.net>
2024-02-23 16:51:31 +0100

    ADD openweather API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  title:string = "Appli Météo";
+  temp?:number;
+  city?:string;
+  apiUrl!:string;
+  data:any;
+  icon?:string;
+  description?:string;
+
+  constructor(private httpClient:HttpClient) {
+    this.apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=saint-lo&lang=fr&units=metric&appid=";
+
+    // console.log(this.httpClient.get(this.apiUrl));
+    this.getMeteo();
+  }
+
+  async getMeteo(){
+    this.readApi(this.apiUrl).subscribe((data) => {
+      this.data = data;
+      console.log(this.data);
+      this.city = this.data.name;
+      this.temp = Math.round(this.data.main.temp);
+      this.icon = this.data.weather[0].icon;
+      this.description = this.data.weather[0].description
+    })
+  }
+
+  readApi(url:string){
+    return this.httpClient.get(url);
+  }
 
 }
